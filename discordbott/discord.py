ec628f0e8f7d46858fa045411adc9fdd4f829fbc
Fernando Galvan <n.brighter00@gmail.com>
2022-08-06 16:48:48 -0500

    basic functions

--- discordbott/discord.py
+++ discordbott/discord.py
@@ -1,1 +1,14 @@
 import discord
+
+client = discord.client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+        
