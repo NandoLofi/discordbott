e8526df7b0bd04a3fe902b9257ca3adf3189450e
Fernando Galvan <n.brighter00@gmail.com>
2022-08-07 15:05:41 -0500

    changed API but need to restructure

--- discordbott/funfact.py
+++ discordbott/funfact.py
@@ -8,15 +8,15 @@ limit = 1
 
 TOKEN = os.getenv('TOKEN')
 client = discord.Client()
-URL = "https://zenquotes.io/api/random/"
+URL = "	https://api.api-ninjas.com/v1/facts"
 API_KEY = os.getenv('API_KEY')
 
 def get_ff():
-    response = requests.get(URL)
-    json_data = json.loads(response.text)
-    quote = json_data[0]['q']
-    return(quote)
-    
+    response = requests.get(URL, headers={'X-Api-Key': API_KEY})
+    if response.status_code == requests.codes.ok:
+        return(response.text)
+    else:
+        print("error")
 
 
 @client.event
@@ -31,9 +31,9 @@ async def on_message(message):
     if message.content.startswith('!ready'):
         await message.channel.send("Ready to provide some fun facts!")
 
-    if message.content.startswith('!quote'):
-        quote = get_ff()
-        await message.channel.send(quote)
+    if message.content.startswith('!fact'):
+        fact = get_ff()
+        await message.channel.send(fact)
     
 
 
