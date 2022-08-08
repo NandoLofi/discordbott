c9438d899a79b3071e1a50ffeca2f7a7240c8f0a
Fernando Galvan <n.brighter00@gmail.com>
2022-08-08 16:51:09 -0500

    adding more functionality and facts

--- discordbott/funfact.py
+++ discordbott/funfact.py
@@ -5,12 +5,15 @@ load_dotenv()
 import requests
 import json
 from types import SimpleNamespace
-limit = 1
+import random 
 
 TOKEN = os.getenv('TOKEN')
 client = discord.Client()
 URL = "	https://api.api-ninjas.com/v1/facts"
 API_KEY = os.getenv('API_KEY')
+
+
+favorite_facts = ["The human body literally glows", "The moon has moon quakes", "Copper knobs are self-disinfecting", "Stars only twinkle due to our atmosphere", "Kids ask 300 questions a day on average", "The first airplane flew on December 17, 1903"]
 
 def get_ff():
     response = requests.get(URL, headers={'X-Api-Key': API_KEY})
@@ -24,19 +27,21 @@ def get_ff():
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    print(get_ff())
+    
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!ready'):
-        await message.channel.send("Ready to provide some fun facts!")
+    if message.content.startswith('!favorite'):
+        await message.channel.send(random.choice(favorite_facts))
 
     if message.content.startswith('!fact'):
         fact = get_ff()
         await message.channel.send(fact)
+    
+
     
 
 
