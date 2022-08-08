31fe69cb7e8d5e723f46018e6504104005581f9b
Fernando Galvan <n.brighter00@gmail.com>
2022-08-08 12:08:34 -0500

    tried namespace method for parsing

--- discordbott/funfact.py
+++ discordbott/funfact.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 import requests
 import json
+from types import SimpleNamespace
 limit = 1
 
 TOKEN = os.getenv('TOKEN')
@@ -14,7 +15,8 @@ API_KEY = os.getenv('API_KEY')
 def get_ff():
     response = requests.get(URL, headers={'X-Api-Key': API_KEY})
     if response.status_code == requests.codes.ok:
-        return(response.text)
+        json_data = json.loads(response.text, object_hook=lambda d: SimpleNamespace(**d))
+        return(json_data[0].fact)
     else:
         print("error")
 
@@ -22,6 +24,7 @@ def get_ff():
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    print(get_ff())
 
 @client.event
 async def on_message(message):
